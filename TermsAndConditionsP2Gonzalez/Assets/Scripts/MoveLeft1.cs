c662a52bd3f1c79296e41f1ba946e01630f94f35
johnathann <johnmail123102@gmail.com>
2023-05-17 09:46:49 -0700

    reworked scripts
    finished adding scripts and reworked so they work kinda

---
+++ TermsAndConditionsP2Gonzalez/Assets/Scripts/MoveLeft1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft1 : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        }
+
+        if (transform.position.x < leftBound -10 && gameObject.CompareTag("Obsticle"))
+        {
+            Destroy(gameObject);
+        }
+    }      
+}
