26b992c561210e49ca1a45c018d3653effea98b7
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2023-05-25 11:58:58 -0700

    stuck on UI

--- TermsAndConditionsP2Gonzalez/Assets/Scripts/gameManager.cs
+++ TermsAndConditionsP2Gonzalez/Assets/Scripts/gameManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SpawnManager : MonoBehaviour
+
+public class GameManager : MonoBehaviour
 {
     
     public GameObject[] obstaclePrefab;
@@ -10,12 +14,36 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    public GameObject titleScreen;
+    public bool isGameActive;
+    private int score;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnObstacle());
+        UpdateScore(0);
+        
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
@@ -32,5 +60,7 @@ public class SpawnManager : MonoBehaviour
             Instantiate(obstaclePrefab[animalIndex], new Vector3(30, 1, 0), obstaclePrefab[animalIndex].transform.rotation);
         }
     }
+
+   
 
 }
